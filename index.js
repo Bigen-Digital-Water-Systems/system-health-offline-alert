4e3937dba3192acbf7f8e60b5c750b4916205801
Hanno Vorster <vorster.hanno@protonmail.com>
2024-06-27 13:29:41 +0200

    Bug fix

--- index.js
+++ index.js
@@ -23,13 +23,20 @@ function checkDevicesUptime(devices) {
   // Loop through all loggers aand check which devices are offline
   for (let device of devices) {
     // Push to array
-    let diffMinutes = moment.duration(moment().diff(moment(device.DateTime))).asMinutes();
-    if (diffMinutes >= parseInt(device.KeepAliveMinutes) && diffMinutes <= parseInt(device.KeepAliveMinutes) + 5) {
+    let diffMinutes = moment
+      .duration(moment().diff(moment(device.DateTime)))
+      .asMinutes();
+    if (
+      diffMinutes >= parseInt(device.KeepAliveMinutes) &&
+      diffMinutes <= parseInt(device.KeepAliveMinutes) + 5
+    ) {
       bulk.push({
         DeviceId: device.LoggerId,
         Name: device.Name,
         LastMessage: moment(device.DateTime).format("YYYY-MM-DD HH:mm"),
-        DiffMinutes: moment.duration(moment().diff(moment(device.DateTime))).asMinutes(),
+        DiffMinutes: moment
+          .duration(moment().diff(moment(device.DateTime)))
+          .asMinutes(),
         SignalStrength: device.SignalStrength,
         Battery: device.Battery,
       });
@@ -40,14 +47,18 @@ function checkDevicesUptime(devices) {
   if (bulk.length > 0) {
     let html = `<ul>`;
     bulk.forEach((device) => {
-      html += `<li>${device.Name} (${device.DeviceId})<br>Last Message: ${moment(device.DateTime)
-        .add(2, "h")
-        .format("YYYY-MM-DD HH:mm")}<br>Battery: ${device.Battery}<br>Signal: ${device.Lqi}</li>`;
+      html += `<li>${device.Name} (${
+        device.DeviceId
+      })<br>Last Message: ${moment(device.DateTime).format(
+        "YYYY-MM-DD HH:mm"
+      )}<br>Battery: ${device.Battery}<br>Signal: ${device.Lqi}</li>`;
     });
     html += `</ul>`;
 
     // Populate email template
-    var emailHtml = fs.readFileSync(process.cwd() + "/templates/device-offline.html").toString();
+    var emailHtml = fs
+      .readFileSync(process.cwd() + "/templates/device-offline.html")
+      .toString();
     emailHtml = emailHtml.replaceAll("{{0}}", html);
 
     // Send email
